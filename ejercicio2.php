4f2697f30a63427f18c0d415dad772aed8b7c994
jmolinafi18dw <jmolinafi18dw@ikzubirimanteo.com>
2019-09-13 09:51:13 +0200

    Ejercicio 2 terminado

---
+++ ejercicio2.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>Hola mundo</title>
+ </head>
+ <body>
+ <?php 
+ //Tiene que salir el numero 52 en pantalla
+ echo max(8, 35, 51, 8, 17, 52);
+ ?>
+ </body>
+</html>
---
+++ ejercicio3.php
@@ -0,0 +1,20 @@
+<html>
+ <head>
+  <title>Hola mundo</title>
+ </head>
+ <body>
+ <?php
+ $edad=61;
+ switch ($var) {
+ case value1:
+ instruccion1;
+ break;
+ case value2:
+ instruccion2;
+ break;
+ default:
+ instruccion;
+ }
+ ?>
+ </body>
+</html>
